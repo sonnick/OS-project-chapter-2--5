a7473174d744892699cd113b4879ef33e8b2f997
sonnick <kdicknison@gmail.com>
2021-11-09 14:20:41 -0500

    Create schedule_sjf.c

---
+++ schedule_sjf.c
@@ -0,0 +1,15 @@
+//
+#include "schedulers.h"
+
+#define MIN_PRIORITY 1
+#define MAX_PRIORITY 10
+
+// create a task to the list
+void create(char *name, int priority, int burst){
+
+}
+
+// invoke the scheduler
+void schedule(){
+
+}
